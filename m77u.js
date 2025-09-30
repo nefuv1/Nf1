1db14c82aad3163547468b0abb18af72614c72c6
nefuv1 <alipbilal52@gmail.com>
2025-09-30 10:15:00 +0700

    upload: m77u.js

---
+++ m77u.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+const config = {
+    owner: ["238594123935954", "6283143776050"],
+    thumbnail: './assets/menu.jpg',
+    ownerName: "LippWangsaff",
+    botNumber: "6283146091646",
+    gemini_api_keys: [
+        "AIzaSyAISxq0MDdS9eKC7-u9wZGQO2Iy2FNYcu0",
+        "AIzaSyCC_-UrJx-mwj7Xn93SjOULp_O3n2KJ1uI"
+    ]
+};
+
+export default config;
